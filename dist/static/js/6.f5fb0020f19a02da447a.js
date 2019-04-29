86d70b863611b969e09dac694d425a9a3cfe73d2
André Rincón Luna <arinconl@tulane.edu>
2019-04-29 13:18:54 -0500

    dist added

---
+++ dist/static/js/6.f5fb0020f19a02da447a.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{yL53:function(e,s,t){"use strict";Object.defineProperty(s,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,s=this._self._c||e;return s("div",[s("b-alert",{attrs:{variant:"success",show:""}},[this._v(this._s(this.message))]),this._v(" "),s("br")],1)},staticRenderFns:[]},n=t("VU/8")({props:["message"]},r,!1,null,null,null);s.default=n.exports}});
+//# sourceMappingURL=6.f5fb0020f19a02da447a.js.map
---
+++ dist/static/js/7.2f8db0c7a142dbfa1628.js
@@ -0,0 +1,2 @@
+webpackJsonp([7],{c27y:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("h2",[this._v("About")]),this._v(" "),t("p",[this._v("Tool for creating petal sheet metal")])])}]},s=n("VU/8")(null,l,!1,null,null,null);t.default=s.exports}});
+//# sourceMappingURL=7.2f8db0c7a142dbfa1628.js.map
